52321ca76d9b1456b5858dc04dbf92c316c43085
Suraj Maity <surajmaity981@gmail.com>
2023-11-09 09:57:24 +0530

    Project structure changed [1]

--- src/main/java/com/averysync/shcmsbackend/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.averysync.shcmsbackend.repository;
-
-import com.averysync.shcmsbackend.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
