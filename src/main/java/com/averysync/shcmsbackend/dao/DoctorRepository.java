d8b95a03e5cc38181c4b693b6f0c726da7d44809
Suraj Maity <surajmaity981@gmail.com>
2023-11-10 23:36:16 +0530

    feat: Find by first name and department wise

--- src/main/java/com/averysync/shcmsbackend/dao/DoctorRepository.java
+++ src/main/java/com/averysync/shcmsbackend/dao/DoctorRepository.java
@@ -1,7 +1,12 @@
 package com.averysync.shcmsbackend.dao;
 
 import com.averysync.shcmsbackend.entity.Doctor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    Page<Doctor> findByFirstNameContaining(@RequestParam("firstName") String firstName, Pageable pageable);
+    Page<Doctor> findByDept(@RequestParam("dept") String dept, Pageable pageable);
 }
